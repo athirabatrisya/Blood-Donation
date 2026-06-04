58b82f65ebaea943233087ebe5980db5afbd7598
athirabatrisya <137164179+athirabatrisya@users.noreply.github.com>
2026-06-04 22:43:31 +0800

    Fix import statement for streamlit_option_menu

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 import plotly.express as px
-from streamlit_option_menu import option_menu
+import streamlit_option_menu
 
 # ─────────────────────────────────────────────────────────────────────────────
 # CONFIGURATION
